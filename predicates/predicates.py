e1ade585b3776c46bb3e0dad5050a8cd1c9e870f
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2021-12-20 08:22:02 -0800

    first push

---
+++ predicates/predicates.py
@@ -0,0 +1,76 @@
+import csv
+import bmt
+import requests
+from biothings_explorer.smartapi_kg.dataload import load_specs
+
+tk = bmt.Toolkit('https://raw.githubusercontent.com/biolink/biolink-model/2.2.12/biolink-model.yaml')
+tsv_file = open("predicates.tsv", "w")
+tsv_attributes = open("attributes.tsv", "w")
+tsv_writer = csv.writer(tsv_file, delimiter='\t')
+tsv_writer_att = csv.writer(tsv_attributes, delimiter='\t')
+
+def sample_predicates():
+    specs = load_specs()
+    kp_titles = []
+    for spec in specs:
+        if 'x-translator' not in spec['info']:
+            continue
+        if spec['info']['x-translator']['component'] == 'KP':
+            kp_titles.append(spec['info']['title'])
+        if 'servers' not in spec:
+            continue
+        else:
+            url = spec['servers'][0]['url']
+            apititle = '_'.join(spec['info']['title'].split())
+            if url.endswith('/'):
+                url = url[:-1]
+            predicates_url = f'{url}/meta_knowledge_graph'
+            print(predicates_url)
+            trapi, predicates = get_predicates(predicates_url)
+            # if is_trapi:
+            if not predicates:
+                continue
+            else:
+                preds, attribs, url = dump_trapi_predicate_results(predicates, predicates_url)
+                predicates = set(preds)
+                attributes = set(attribs)
+                for pred in predicates:
+                    tsv_writer.writerow([apititle, url, pred])
+                for attrib in attributes:
+                    tsv_writer_att.writerow([apititle, url, attrib])
+            # else:
+            #     dump_smartapi_predicate_results(spec['info']['title'])
+
+
+def in_biolink_model(predicate):
+    is_predicate = tk.is_predicate(predicate)
+    return is_predicate
+
+
+def dump_trapi_predicate_results(predicates, url):
+    preds = []
+    attribs = []
+    for edge in predicates.get('edges'):
+        predicate = edge.get('predicate')
+        if 'attributes' in edge and edge.get('attributes') is not None:
+            for attribute in edge.get('attributes'):
+                attribs.append(attribute.get('attribute_type_id'))
+        preds.append(predicate)
+    return preds, attribs, url
+
+            # tsv_writer.writerow([subject, predicate, tobject, url])
+
+
+def get_predicates(pr_url):
+    try:
+        response = requests.get(pr_url)
+        if response.status_code == 200:
+            return True, response.json()
+        else:
+            return False, {}
+    except:
+        return False, {}
+
+
+if __name__ == '__main__':
+    sample_predicates()
